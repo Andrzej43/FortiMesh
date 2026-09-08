2541e41184620963f86d0dadd3a8f7d0c836dc29
m0dE <61428067+bboym0de@users.noreply.github.com>
2026-09-08 16:21:44 +1000

    Delete src directory

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-// src/App.test.js
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders FortiMesh title', () => {
-    render(<App />);
-    const titleElement = screen.getByText(/FortiMesh/i);
-    expect(titleElement).toBeInTheDocument();
-});
